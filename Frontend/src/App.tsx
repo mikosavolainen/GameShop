c8de47ee106d0bac52b76d2d48a50a42e28bd104
Sini Petlin <sini.petlin@edu.salpaus.fi>
2024-08-27 12:56:34 +0300

    testing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <>
       <p className='text-5xl text-wrench-neutral-white pt-40 font-bold'>1 200 500+ games of any kind</p>
-      <Input type='text' width={62} size='normal-white' icon='search' label='Search' placeholder='Start your search here'/>
+      <Input type='password' width={62} size_color='normal-white' icon='key' label='Search' placeholder='Start your search here' />
+      <Input type='text' width={62} size_color='small-dark' icon='person' label='Search' placeholder='Start your search here' />
+      <Input type='text' width={62} size_color='normal-dark' icon='mail' label='Search' placeholder='Start your search here' />
+      <Input type='text' width={62} size_color='small-white' icon='search' label='Search' placeholder='Start your search here' />
     </>
   )
 }
--- Frontend/src/components/input.tsx
+++ Frontend/src/components/input.tsx
@@ -1,7 +1,14 @@
 import '../App.css'
-export default function Input({placeholder, className, label, width, icon, size, type}:{placeholder: string, className?: string, size: "small-white" | "normal-white" | "small-dark" | "normal-dark", type: "text" | "password", label?: string, icon: "search", width: number}){
+export default function Input({placeholder, className, label, width, icon, size_color, type, onChange}:{placeholder: string,
+                                                                                                        className?: string,
+                                                                                                        label?: string,
+                                                                                                        width: number,
+                                                                                                        icon: "search" | "key" | "person" | "mail",
+                                                                                                        size_color: "small-white" | "normal-white" | "small-dark" | "normal-dark",
+                                                                                                        type: "text" | "password",
+                                                                                                        onChange?: React.ChangeEventHandler<HTMLInputElement> }){
     let string = ""
-    switch (size){
+    switch (size_color){
         case "small-white":
             string = "bg-wrench-neutral-white rounded-full border border-wrench-neutral-3 text-wrench-neutral-dark text-base p-1 pl-10 focus:border-wrench-neutral-2 outline-none"
             break
@@ -19,11 +26,11 @@ export default function Input({placeholder, className, label, width, icon, size,
         <>
         <div>
         {
-            label && <label className='text-base block m-0 mb-1 text-wrench-neutral-white'>{label}</label>
+            label && <label className='text-base block m-0 mt-1 mb-1 text-wrench-neutral-white'>{label}</label>
         }
             <div className={`${className} relative`}>
             <span className="text-wrench-neutral-3 material-icons block absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none">{icon}</span>
-            <input type={`${type}`} className={`${string}`} placeholder={`${placeholder}`} size={width}/>
+            <input onChange={onChange} type={`${type}`} className={`${string}`} placeholder={`${placeholder}`} size={width}/>
             </div>
         </div>
         </>
