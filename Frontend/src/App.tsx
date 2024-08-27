dc87b5158b4ff0c21107972f9ac8fd525c7f6390
Sini Petlin <sini.petlin@edu.salpaus.fi>
2024-08-27 13:11:04 +0300

    changes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,10 +5,7 @@ function App() {
   return (
     <>
       <p className='text-5xl text-wrench-neutral-white pt-40 font-bold'>1 200 500+ games of any kind</p>
-      <Input type='password' width={62} size_color='normal-white' icon='key' label='Search' placeholder='Start your search here' />
-      <Input type='text' width={62} size_color='small-dark' icon='person' label='Search' placeholder='Start your search here' />
-      <Input type='text' width={62} size_color='normal-dark' icon='mail' label='Search' placeholder='Start your search here' />
-      <Input type='text' width={62} size_color='small-white' icon='search' label='Search' placeholder='Start your search here' />
+      <Input type='text' width={62} size_color='normal-white' icon='search' label='Search' placeholder='Start your search here' />
     </>
   )
 }
