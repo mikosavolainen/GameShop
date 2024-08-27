0790fcd54626389682549a066e74d597645ecafe
Sini Petlin <sini.petlin@edu.salpaus.fi>
2024-08-27 11:57:36 +0300

    input component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,13 +4,7 @@ import Input from './components/input.tsx'
 function App() {
   return (
     <>
-      <h1 className='text-green-400'>
-        hi
-      </h1>
-      <Input type="small-white" />
-      <Input type="normal-white"/>
-      <Input type="small-dark"/>
-      <Input type="normal-dark"/>
+      <Input type='text' width={20} size='small-white' icon='search' label='nice'/>
     </>
   )
 }
--- Frontend/src/components/input.tsx
+++ Frontend/src/components/input.tsx
@@ -1,23 +1,31 @@
 import '../App.css'
-export default function Input({type}:{type: "small-white" | "normal-white" | "small-dark" | "normal-dark"}){
-    let string = "";
-    switch (type){
+export default function Input({className, label, width, icon, size, type}:{className?: string, size: "small-white" | "normal-white" | "small-dark" | "normal-dark", type: "text" | "password", label?: string, icon: "search", width: number}){
+    let string = ""
+    switch (size){
         case "small-white":
-            string = "bg-wrench-neutral-white rounded-full border border-wrench-neutral-3 text-wrench-neutral-2 text-16 p-1"
+            string = "bg-wrench-neutral-white rounded-full border border-wrench-neutral-3 text-wrench-neutral-dark text-16 p-1 pl-10 focus:border-wrench-neutral-2 outline-none"
             break
         case "normal-white":
-            string = "bg-wrench-neutral-white rounded-full border border-wrench-neutral-3 text-wrench-neutral-2 text-20 p-2"
+            string = "bg-wrench-neutral-white rounded-full border border-wrench-neutral-3 text-wrench-neutral-dark text-20 p-2 pl-10 focus:border-wrench-neutral-2 outline-none"
             break
         case "small-dark":
-            string = "bg-wrench-neutral-4 rounded-full border border-wrench-neutral-3 text-wrench-neutral-2 text-16 p-1"
+            string = "bg-wrench-neutral-4 rounded-full border border-wrench-neutral-3 text-wrench-neutral-white text-16 p-1 pl-10 focus:border-wrench-neutral-2 outline-none"
             break
         case "normal-dark":
-            string = "bg-wrench-neutral-4 rounded-full border border-wrench-neutral-3 text-wrench-neutral-2 text-20 p-2"
+            string = "bg-wrench-neutral-4 rounded-full border border-wrench-neutral-3 text-wrench-neutral-white text-20 p-2 pl-10 focus:border-wrench-neutral-2 outline-none"
             break
     }
     return(
         <>
-        <input className={`${string}`} placeholder="Input placeholder"></input>
+        <div>
+        {
+            label && <label className='m-0'>{label}</label>
+        }
+            <div className={`${className} relative`}>
+            <span className="text-wrench-neutral-3 material-icons block absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none text-xs">{icon}</span>
+            <input type={`${type}`} className={`${string}`} placeholder="Input placeholder" size={width}/>
+            </div>
+        </div>
         </>
     )      
 }
