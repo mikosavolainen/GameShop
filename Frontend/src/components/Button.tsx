fbe9864ecf4ec4c4bde02c966ea1e46cf49ae5ce
ptchtrns <ptchtrns@gmail.com>
2024-09-02 09:48:59 +0300

    button changes

--- Frontend/src/components/Button.tsx
+++ Frontend/src/components/Button.tsx
@@ -6,12 +6,14 @@ export default function Button({
   size = 'small',
   icon,
   text,
+  disabled
 }: {
   type: 'link' | 'button' | 'submit'
   style?: 'purple' | 'coral' | 'neutral'
   size?: 'small' | 'big'
   icon: string
   text: string
+  disabled?: boolean
 }) {
   return (
     <div
@@ -20,24 +22,26 @@ export default function Button({
           case 'small':
             return 'border p-1'
           case 'big':
-            return 'border'
+            return 'border text-xl'
         }
       })()} ${(() => {
         switch (style) {
           case 'neutral':
-            return 'border-wrench-neutral-3'
+            return `${ disabled ? 'text-wrench-neutral-3' : 'border-wrench-neutral-2 text-wrench-neutral-white' }`
           case 'purple':
-            return 'text-xl'
+            return `border-wrench-purple-2 ${ disabled ? 'text-wrench-neutral-2 bg-wrench-purple-2' : 'border-wrench-neutral-white bg-wrench-purple text-wrench-neutral-white' }`
+          case 'coral':
+            return `border-wrench-coral-2 ${ disabled ? 'text-wrench-neutral-2 bg-wrench-coral-2' : 'border-wrench-neutral-white bg-wrench-coral text-wrench-neutral-white' }`
         }
-      })()} bg-white inline-block rounded-full relative`}
+      })()} bg-white inline-block rounded-full relative ${ !disabled && 'shadow-[inset_0_1px_0_rgba(216,191,216,0.15)]' } active:shadow-none`}
     >
-      <span className="text-wrench-neutral-white material-icons block absolute left-2 top-1/2 -translate-y-1/2 pointer-events-none">
+      <span className="material-icons block absolute left-2 top-1/2 -translate-y-1/2 pointer-events-none">
         {icon}
       </span>
       {(() => {
         switch (type) {
           case 'link':
-            return <Link href="https://google.com" className="text-wrench-neutral-white pl-7 pr-2">{text}</Link>
+            return <Link href="https://google.com" className="pl-7 pr-2">{text}</Link>
           case 'button':
           case 'submit':
             return <button>{text}</button>
