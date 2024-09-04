712c36cafd8e0cfb87d3141be8e4fae8491c45c0
Sini Petlin <sini.petlin@edu.salpaus.fi>
2024-09-04 13:04:21 +0300

    smaller change

--- Frontend/src/components/Popular.tsx
+++ Frontend/src/components/Popular.tsx
@@ -4,7 +4,7 @@ export default function Popular(){
     return(
         <>
             <p className="text-2xl pt-10">Popular</p>
-            <div className="pb-20 grid sm-:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+            <div className="pb-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
                 <GameDisplay classname="pb-5" game_name="gameName" price={40} discount={20} description="Test test test test test test test test test test test test test test test test test test test" size="small" categories={["Cool"]}/>
                 <GameDisplay classname="pb-5" game_name="gameName" price={40} description="Test" size="small" categories={["Cool"]}/>
                 <GameDisplay classname="pb-5" game_name="gameName" price={40} description="Test" size="small" categories={["Cool"]}/>
