5ef80f801b0363216bbd9301518e8a9849656517
ptchtrns <ptchtrns@gmail.com>
2024-09-17 09:59:17 +0300

    manually added filed from Backend branch

--- Backend/index.js
+++ Backend/index.js
@@ -16,10 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Mongoose
-mongoose.connect("mongodb://Kissa:KissaKala2146@37.219.151.14:27018/Wrench", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://Kissa:KissaKala2146@37.219.151.14:27018/Wrench");
 
 const db = mongoose.connection;
 
@@ -35,9 +32,9 @@ const userSchema = new mongoose.Schema({
 	password: { type: String, required: true },
 	confirmedemail: { type: Boolean, default: false },
 });
-const User = mongoose.model("User", userSchema);
+const users = mongoose.model("users", userSchema);
 
-const GamesSchema = new mongoose.Schema({
+const gamesSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	desc: { type: String },
 	gamefileloc: { type: String },
@@ -45,8 +42,27 @@ const GamesSchema = new mongoose.Schema({
 	category: { type: Array },
 	price: { type: Number },
 	ratings: { type: Array },
+	multiplayer: { type: String },
+	Picturefileloc: { type: String },
 });
-const Games = mongoose.model("Games", GamesSchema);
+const games = mongoose.model("games", gamesSchema);
+
+const ReviewsSchema = new mongoose.Schema({
+	game: { type: String },
+	date: { type: Number },
+	writer: { type: String },
+	ratings: { type: Array },
+	desc: { type: String },
+});
+const Reviews = mongoose.model("Reviews", ReviewsSchema);
+
+const LibrarySchema = new mongoose.Schema({
+  owner: { type: String},
+  game: { type: String },
+
+});
+const Library = mongoose.model("Library", LibrarySchema);
+
 
 const convertUsernameToLowerCase = (req, res, next) => {
 	if (req.body.username) {
@@ -86,30 +102,64 @@ app.get("/", (req, res) => {
 	res.json("hello world");
 });
 
+app.post("/get-all-owned-games", async (req, res) => {
+  try {
+    const ownerId = req.body.owner; 
+    if (!ownerId) {
+      return res.status(400).send("Owner ID is required");
+    }
+   
+    const games = await Library.find({ owner: ownerId });
+
+    res.json(games);
+  } catch (error) {
+    console.error("Error fetching games:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+app.post("/get-all-games", async (req, res) => {
+	try {
+		const game = await games.find();
+		return res.json(game);
+	} catch (error) {
+		console.error("Error fetching games:", error);
+		return res.status(500).send("Internal Server Error");
+	}
+});
+
 app.get("/confirm", async (req, res) => {
 	const jwts = req.query.confirm;
 	if (jwts) {
 		try {
 			const { username } = jwt.verify(jwts, SECRET_KEY);
-			await User.findOneAndUpdate({ username }, { confirmedemail: true });
-			res.send("Email confirmed successfully.");
+			await users.findOneAndUpdate(
+				{ username },
+				{ confirmedemail: true }
+			);
+			return res.send("Email confirmed successfully.");
 		} catch (error) {
-			res.status(400).send("Invalid or expired confirmation link.");
+			return res
+				.status(400)
+				.send("Invalid or expired confirmation link.");
 		}
 	} else {
-		res.status(400).send("Confirmation token is missing.");
+		return res.status(400).send("Confirmation token is missing.");
 	}
 });
 
 // Rekisteröinti
 app.post("/register", convertUsernameToLowerCase, async (req, res) => {
 	const { username, password, email, phonenumber } = req.body;
-	const existingUser = await User.findOne({ username });
+	const existingUser = await users.findOne({
+		$or: [{ username: username }, { email: email }],
+	});
 	if (existingUser) {
-		return res.status(409).send("Username already exists");
+		return res.status(409).send("Email or username already exists");
 	}
 	const hashedPassword = await bcrypt.hash(password, 10);
-	const newUser = new User({
+	const newUser = new users({
 		username,
 		password: hashedPassword,
 		email,
@@ -195,7 +245,7 @@ app.post("/register", convertUsernameToLowerCase, async (req, res) => {
 
 app.post("/login", convertUsernameToLowerCase, async (req, res) => {
 	const { username, password } = req.body;
-	const user = await User.findOne({
+	const user = await users.findOne({
 		$or: [{ username: username }, { email: username }],
 	});
 	if (!user || !bcrypt.compareSync(password, user.password)) {
@@ -208,57 +258,86 @@ app.post("/login", convertUsernameToLowerCase, async (req, res) => {
 	const token = jwt.sign({ uname }, SECRET_KEY, { expiresIn: "1h" });
 	res.json({ token });
 });
-app.post("/forgot-password", async (req, res) => {
-	const { email } = req.body;
-	const user = await User.findOne({ email });
-	if (!user) {
-		res.status(400).send("didnt find email");
+app.post("/reset-password", async (req, res) => {
+	const { token, password } = req.query;
+	const x = jwt.verify(token, SECRET_KEY);
+	if (x) {
+		const newpass = await bcrypt.hash(password, 10);
+		await users.findOneAndUpdate({ email: x.email }, { password: newpass });
+		return res.status(200).send("RESTED");
 	}
+});
+app.post("/forgot-password", convertUsernameToLowerCase, async (req, res) => {
+	const { email } = req.body;
+	const user = await users.findOne({ email: email });
+
+	if (!user) {
+		return res.status(400).send("didnt find email");
+	}
+	if (!user.confirmedemail) {
+		return res.status(401).send("You need to confirm your email");
+	}
+	const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: "15min" });
+
 	confirmation = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Reset Confirmation</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
+            font-family: 'Helvetica Neue', Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f9f9f9;
             color: #333;
-            padding: 20px;
+            line-height: 1.6;
         }
         .container {
             max-width: 600px;
-            margin: 0 auto;
-            background-color: #fff;
+            margin: 40px auto;
             padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            background-color: #ffffff;
+            border-radius: 10px;
+            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+            text-align: center;
         }
         h1 {
+            font-size: 24px;
             color: #333;
-            text-align: center;
+            margin-bottom: 20px;
         }
         p {
-            line-height: 1.6;
+            font-size: 16px;
+            color: #555;
+            margin-bottom: 20px;
         }
         .button {
-            display: block;
-            width: 200px;
-            margin: 20px auto;
-            padding: 10px;
-            background-color: #007BFF;
-            color: white;
-            text-align: center;
+            display: inline-block;
+            position: relative;
+            padding: 12px 35px;
+            margin-top: 20px;
+            font-size: 17px;
+            font-weight: 500;
+            color: #ffffff;
+            background: linear-gradient(145deg, #b0b0b0, #e0e0e0);
+            border: 2px solid #a6a6a6;
+            border-radius: 8px;
             text-decoration: none;
-            border-radius: 5px;
+            transition: background-color 0.3s ease, color 0.3s ease, box-shadow 0.3s ease;
+        }
+        .button:hover {
+            background: linear-gradient(145deg, #e0e0e0, #b0b0b0);
+            color: #333;
+            box-shadow: 0 0 15px rgba(128, 128, 128, 0.5);
         }
         .footer {
-            text-align: center;
-            margin-top: 20px;
-            color: #777;
-            font-size: 12px;
-        }
+                margin-top: 30px;
+                font-size: 12px;
+                color: #999;
+            }
     </style>
 </head>
 <body>
@@ -267,21 +346,21 @@ app.post("/forgot-password", async (req, res) => {
         <p>Hello,</p>
         <p>We received a request to reset the password for your account associated with this email address. If you did not request a password reset, please ignore this email.</p>
         <p>To reset your password, please click the button below:</p>
-        <a href="https://yourwebsite.com/reset-password?token=YOUR_RESET_TOKEN" class="button">Reset Password</a>
+        <a href="http://localhost:5000/reset-password?token=${token}" class="button">Reset Password</a>
         <p>If the button above does not work, please copy and paste the following link into your web browser:</p>
-        <p>https://yourwebsite.com/reset-password?token=YOUR_RESET_TOKEN</p>
+        <p>http://localhost:5000/reset-password?token=${token}</p>
         <p>This link will expire in 24 hours for your security.</p>
-        <p>Thank you,<br>Your Company Name</p>
-    </div>
     <div class="footer">
-        <p>If you did not request this, please ignore this email.</p>
+         <p>Thank you,<br>The Wrench Team</p>
+    </div>
     </div>
 </body>
 </html>
+
 `;
 
 	await sendMail(confirmation, "Password reset", email);
-	res.status(200).send("reset password email send");
+	return res.status(200).send("reset password email send");
 });
 app.post("/upload", async (req, res) => {
 	if (req.file) {
