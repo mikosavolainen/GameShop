feff567707dd33c7bc07acf1c55427bb05358878
ptchtrns <ptchtrns@gmail.com>
2024-09-25 10:07:29 +0300

    fixed small bug

--- Frontend/src/components/Checkbox.tsx
+++ Frontend/src/components/Checkbox.tsx
@@ -1,7 +1,7 @@
 const Checkbox = ({ label, id, name, className, error, required = false}: { label: string, id: string, name?: string, className?: string, error?: string, required: boolean}) => {
   return (
     <div className={`${className}`}>
-      <div className={`flex items-center gap-2 relative hover:cursor-pointer`}>
+      <div className={`inline-flex items-center gap-2 relative hover:cursor-pointer`}>
         <input
           type="checkbox"
           id={id}
