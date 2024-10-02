9b4fbddba6418ddd2f241bbbef161324dad40423
ptchtrns <ptchtrns@gmail.com>
2024-10-02 13:19:12 +0300

    checkout work in progress

--- Frontend/src/components/Checkout.tsx
+++ Frontend/src/components/Checkout.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 import Footer from "./Parts/Footer"
 
 const Checkout = () => {
+  const [checkout, setCheckout] = useState<string[]>(JSON.parse(localStorage.getItem("checkout") as string) || [])
   return (
     <div className="content-layout-margin mb-16 md:mb-0 md:mt-16 pt-16">
       <div>
         <h1 className='text-5xl text-wrench-neutral-white mt-12 font-bold'>Checkout</h1>
+      </div>
+      <div>
+        {checkout.map(item => <div className="">{item}</div>)}
       </div>
       <Footer />
     </div>
