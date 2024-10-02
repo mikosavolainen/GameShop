b8e4d4f915e5bae19c8e90069019cd5df8e79793
Laberry12 <lauri.mt.kallio@gmail.com>
2024-10-02 13:14:22 +0300

    search fixed

--- Frontend/src/components/SearchFilter.tsx
+++ Frontend/src/components/SearchFilter.tsx
@@ -25,7 +25,7 @@ export default function SearchFilter(){
         const developers: string[] = []
         const fetch = async () => {
           const apiUrl = import.meta.env.VITE_SERVER_BASE_API_URL; // Ensure this environment variable is correctly set
-          const { data } = await axios.get(`${apiUrl}/get-all-games`); // idk why post is used on server side instead of get but ok
+          const { data } = await axios.get(`${apiUrl}/get-all-games`, {params: {limit: 20000, page:1}}); // idk why post is used on server side instead of get but ok
           console.log(data)
           data.map((r: { category: [], author: string }) => {
               developers.push(r.author)
@@ -43,7 +43,7 @@ export default function SearchFilter(){
     useEffect(() => {
         const fetch = async () => {
           const apiUrl = import.meta.env.VITE_SERVER_BASE_API_URL; // Ensure this environment variable is correctly set
-          const { data } = await axios.get(`${apiUrl}/search-game?${searchString}`); // idk why post is used on server side instead of get but ok
+          const { data } = await axios.get(`${apiUrl}/search-game?${searchString}&limit=10&page=1`); // idk why post is used on server side instead of get but ok
           setRes(data)
         }
     
