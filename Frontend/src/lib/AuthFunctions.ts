797696b86d9dfeb7c92fe3ddb03833d0f06d17b1
ptchtrns <ptchtrns@gmail.com>
2024-10-02 10:06:19 +0300

    updated signInHelper

--- Frontend/src/lib/AuthFunctions.ts
+++ Frontend/src/lib/AuthFunctions.ts
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export function signOutHelper(setUser: Dispatch<SetStateAction<{username: string} | null>>) {
   localStorage.removeItem("username");
@@ -6,8 +7,13 @@ export function signOutHelper(setUser: Dispatch<SetStateAction<{username: string
   setUser(null)
 }
 
-export function signInHelper(setUser: Dispatch<SetStateAction<{username: string} | null>>, token: string, username: string){
+export function signInHelper(setUser: Dispatch<SetStateAction<{username: string} | null>>, token: string){
   localStorage.setItem("token", token)
-  localStorage.setItem("username", username)
-  setUser({username: username})
+
+  // Decode the JWT token to get the payload
+  const decodedToken: { username: string } = jwtDecode(token);
+
+  localStorage.setItem("username", decodedToken.username)
+
+  setUser({username: decodedToken.username})
 }
